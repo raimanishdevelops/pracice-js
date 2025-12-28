f90c7f898c4745cfcd955fcc178c806a1a8e37e3
Manish <raiman9705@gmail.com>
2025-12-28 15:11:09 +0530

    second commit

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-
+//  this is a function incremnting a variable , let count 
 function printcount(){
     let count=1;
     count++;
@@ -6,3 +6,23 @@ function printcount(){
 
 }
 printcount();
+
+
+// wap const , let
+// assigned baseurl as a connstant , i.e final as in java
+const BASE_URL="www.basecase.com";
+
+//assign a variable and increase the value
+let count=0;
+count = count + 2;
+
+// reassigning a constant to see ERROR!!!
+function reassignBaseUrl(){
+
+const BASE_URL="www.fb.com";
+BASE_URL="1000";
+
+}
+
+reassignBaseUrl();
+
---
+++ exeutejs.js
@@ -0,0 +1,10 @@
+// reassigning a constant to see ERROR!!!
+function rbu(){
+
+const BASE_URL="www.fb.com";
+console.log(BASE_URL);
+// BASE_URL="1000";
+
+}
+
+rbu();
